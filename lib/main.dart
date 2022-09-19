f83fcf0df7786dfa5691a856a8d20aaf18465e6f
Dana Mulyana <danabontot@gmail.com>
2022-09-19 11:24:52 +0700

    statelessWidget dan StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,61 @@ class MyApp extends StatelessWidget{
           title: Text('Hello, World!'),
         ),
         body: Center(
-          child: Text('Hello,World!'),
+          child: BiggerText(
+            text: "Hello world!",
+          ),// Ubah widget Heading ke PerubahanText
         ),
       ),
+    );
+  }
+}
+
+class Heading extends StatelessWidget{
+  final String text; // state text bersifat final
+
+  const Heading({Key? key, required this.text}) : super(key: key); // lalu state text masuk ke constructor
+
+  @override
+  Widget build(BuildContext context){
+    return Text(
+      text,
+      style: const TextStyle(
+        fontSize: 24.0,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
+
+class BiggerText extends StatefulWidget{
+  final String text; // ini parameter widget
+
+  const BiggerText({Key? key, required this.text}) : super(key: key);
+
+  @override
+  _BiggerTextState createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText>{
+  double _textSize = 16.0; //ini state dari Widget ContohStateful
+
+  @override
+  Widget build(BuildContext context){
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text,
+          style: TextStyle(fontSize: _textSize),
+        ),
+        ElevatedButton(
+          child: const Text("perbesar"),
+          onPressed: (){
+            setState(() {
+              _textSize = 32.0;
+            });
+          },
+        )
+      ],
     );
   }
 }
