6f51a38a70488fc35e13dc39aef46eb32c0ce415
Dana Mulyana <danabontot@gmail.com>
2022-09-21 10:00:10 +0700

    MediaQuery

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,42 @@ class MyApp extends StatelessWidget{
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const FirstScreen(),
+      home: const HomePage(),
     );
   }
 }
+
+//MediaQuery
+class HomePage extends StatelessWidget{
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    Size screenSize = MediaQuery.of(context).size;
+    Orientation orientation = MediaQuery.of(context).orientation;
+
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            'Screen width: ${screenSize.width.toStringAsFixed(2)}',
+            style: const TextStyle(color: Colors.white, fontSize: 18),
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            'Orientation: $orientation',
+            style: const TextStyle(color: Colors.white,fontSize: 18),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
+//END MediaQuery
 
 //Navigation
 class FirstScreen extends StatelessWidget{
