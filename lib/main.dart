0c5240996be151961828b06295e171fa2b632078
Dana Mulyana <danabontot@gmail.com>
2022-09-21 09:12:15 +0700

    listview separated & listviewBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,70 @@ class MyApp extends StatelessWidget{
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const ScrollingScreen(),//Panggil FirstScreen di sini
+      home: const ListViewSeparated(),
+    );
+  }
+}
+
+class ListViewSeparated extends StatelessWidget{
+  const ListViewSeparated({Key? key}) : super(key: key);
+
+  final List<int> numberList = const <int>[1,2,3,4,5,6,7,8,9,10];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.separated(
+        itemCount: numberList.length,
+        itemBuilder: (BuildContext context,int index){
+          return Container(
+            height: 250,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              border: Border.all(color: Colors.black),
+            ),
+            child: Center(
+              child: Text(
+                '${numberList[index]}',
+                style: const TextStyle(fontSize: 50),
+              ),
+            ),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index){
+          return const Divider();
+        },
+      )
+    );
+  }
+}
+
+class ListViewBuilder extends StatelessWidget{
+  const ListViewBuilder({Key? key}) : super(key: key);
+
+  final List<int> numberList = const <int>[1,2,3,4,5,6,7,8,9,10];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: numberList.length,
+        itemBuilder: (BuildContext context,int index){
+          return Container(
+            height: 250,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              border: Border.all(color: Colors.black),
+            ),
+            child: Center(
+              child: Text(
+                '${numberList[index]}',
+                style: const TextStyle(fontSize: 50),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
