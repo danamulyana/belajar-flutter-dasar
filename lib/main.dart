dff77f1d4557a9b2467d75c44507dda8b9879dae
Dana Mulyana <danabontot@gmail.com>
2022-09-21 08:52:34 +0700

    ListView Dinamis

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,36 @@ class MyApp extends StatelessWidget{
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const FirstScreen(),//Panggil FirstScreen di sini
+      home: const ScrollingScreen(),//Panggil FirstScreen di sini
+    );
+  }
+}
+
+class ScrollingScreen extends StatelessWidget{
+  const ScrollingScreen({Key? key}) : super(key: key);
+
+  final List<int> numberList = const <int>[1,2,3,4,5,6,7,8,9,10];
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: ListView(
+        children: numberList.map((number) {
+          return Container(
+            height: 250,
+            child: Center(
+              child: Text(
+                '$number',
+                style: const TextStyle(fontSize: 50),
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              border: Border.all(color: Colors.black),
+            ),
+        );
+        }).toList(),
+      ),
     );
   }
 }
