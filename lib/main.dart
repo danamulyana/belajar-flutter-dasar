04ed1bdb0faf5801b801c0b83e71d71deeaf75bd
Dana Mulyana <danabontot@gmail.com>
2022-09-21 10:24:13 +0700

    Responsive Page

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,53 @@ class MyApp extends StatelessWidget{
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: const ResponsivePage(),
     );
   }
 }
+
+//ResponsivePage
+class ResponsivePage extends StatelessWidget{
+  const ResponsivePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints){
+          if(constraints.maxWidth < 600){
+            return ListView(
+              children:
+                _generateContainers(),
+            );
+          }else if(constraints.maxWidth < 900){
+            return GridView.count(
+                crossAxisCount: 2,
+                children: _generateContainers(),
+            );
+          }else{
+            return GridView.count(
+              crossAxisCount: 6,
+              children: _generateContainers(),
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  List<Widget> _generateContainers(){
+    return List<Widget>.generate(20, (index) {
+      return Container(
+        margin: const EdgeInsets.all(8),
+        color: Colors.blueGrey,
+        height: 200,
+      );
+    });
+  }
+}
+//END ResponsivePage
 
 //MediaQuery
 class HomePage extends StatelessWidget{
